129882f75d7c6d13f35df44f8161eb49fad961de
Ansar Ali <ansar@amharctech.com>
2023-08-10 21:05:35 +0600

    new

---
+++ unique-name.js
@@ -0,0 +1,11 @@
+function uniqueName(arr) {
+    const uniqueNameArray = [];
+    arr.forEach(name => {
+        if (!uniqueNameArray.includes(name)) {
+            uniqueNameArray.push(name);
+        }
+    });
+    return uniqueNameArray;
+}
+
+console.log(uniqueName(['a', 'b', 'c', 'd', 'e', 'a', 'b']));
